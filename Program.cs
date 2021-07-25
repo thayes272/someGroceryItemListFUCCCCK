e206aa09e633abd29e602e5fe153f080127404b6
Dax272 <49327226+Dax272@users.noreply.github.com>
2021-07-25 00:21:10 -0700

    adding the project cuz fuccck me
    *plays rocketeer*

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace shoppingListThing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>(); // making list
+            WriteLine("What should you add to the shopping list?"); //asking dumb shit questions
+            for (int i = 0; i < 10; i++)
+            {
+                var input = Console.ReadLine(); // reading input
+                list.Add(input); // adding that shit cuz yks
+            }
+            //input.ToString();
+            Console.Clear();
+            outputList(list);
+
+
+        }
+
+        static void WriteLine(string input)
+        {
+            Console.WriteLine(input);
+        }
+
+        static void outputList(List<string> list)
+        {
+            string output = string.Join(Environment.NewLine, list.ToArray());
+            WriteLine(output);
+        }
+    }
+}
